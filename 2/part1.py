96e7aa141c785ba2213cbf76751ad7ccd27bfec9
bharathcs <bharath.sudheer@gmail.com>
2020-12-03 14:33:28 +0800

    Complete D02P2

--- 2/part1.py
+++ 2/part1.py
@@ -18,11 +18,13 @@ class Line:
         self.letter = parsed[1][0]
         self.pwd = parsed[2]
         self.valid = self.policy_check()
+        self.valid2 = self.policy2()
 
     def __str__(self):
         return (str(self.numpair)
                 + '\t' + str(self.letter)
                 + '\t' + str(self.valid)
+                + '\t' + str(self.valid2)
                 + '\t' + str(self.pwd)
                 + '\t' + str(self.line) + '\n')
 
@@ -37,9 +39,18 @@ class Line:
         end = int(self.numpair[1])
         return start <= count <= end
 
+    def policy2(self):
+        """Checks if the passwords match the second policy
+        """
+        first = self.pwd[int(self.numpair[0]) - 1]
+        second = self.pwd[int(self.numpair[1]) - 1]
+        return (first == self.letter) == (second != self.letter)
+
 
 CLEANED = [Line(i) for i in PWDS]
 
 RESULT = reduce(lambda x, y: x + (1 if y.valid else 0),
                 CLEANED, 0)
-print(RESULT)
+RESULT2 = reduce(lambda x, y: x + (1 if y.valid2 else 0),
+                 CLEANED, 0)
+print(RESULT, RESULT2)
